4dc7dc7d10a7a912893286fc7e79433baf0d090a
Honn <honn@killingar.net>
2011-11-13 21:57:23 +0100

    Adding basic forum folders and models.
    Signed-off-by: Honn <honn@killingar.net>

---
+++ apps/forum/models.py
@@ -0,0 +1,39 @@
+import datetime
+from django.db import models
+from django.db.models import get_model
+
+from core import ThreadedEntry, Entry
+
+
+class Thread(Entry):
+    """
+    A collection of ForumPost:s.
+
+    """
+    title = models.CharField(max_length=100)
+
+
+class ThreadHistory(EntryHistory):
+    """
+    History for the Thread model.
+    
+    """
+    title = models.CharField(max_length=100)
+
+
+class ForumPost(ThreadedEntry):
+    """
+    A post in the forum.
+    
+    """
+    collection = models.ForeignKey(Thread)
+    body = models.TextField()
+
+
+class ForumPostHistory(ThreadedEntryHistory):
+    """
+    History for the ForumPost model.
+    
+    """
+    body = models.TextField()
+
