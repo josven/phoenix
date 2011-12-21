7088b0c9afcdfc32373892825c48ffd97abf2062
Jonas Svensson <jonas.s.svensson@gmail.com>
2011-12-21 01:47:30 +0100

    removed unused app

--- apps/feedback/context_processors.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-from feedback.forms import FeedbackForm
-
-def feedback_form(request):
-    feedback_form = FeedbackForm()
-
-    return {'feedback_form': feedback_form}
-
