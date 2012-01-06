f6de217775e604da62af2e7255f595642067a08b
Jonas Svensson <jonas.s.svensson@gmail.com>
2012-01-06 04:33:57 +0100

    multiple words noe works in tags

--- apps/forum/urls.py
+++ apps/forum/urls.py
@@ -5,11 +5,11 @@ urlpatterns = patterns('',
     url(r'^$', 'apps.forum.views.read_forum', name='read_forum'),
     
     url(r'^thread/create/$', 'apps.forum.views.create_thread', name='create_thread'),
-    url(r'^thread/create/(?P<tags>(\w+)(,\s*\w+)*)/', 'apps.forum.views.create_thread', name='create_thread_by_tags'),
+    url(r'^thread/create/(?P<tags>(.+)(,\s*.+)*)/', 'apps.forum.views.create_thread', name='create_thread_by_tags'),
 
     url(r'^thread/read/(?P<id>\d*)/$', 'apps.forum.views.read_thread', name='read_thread'),
     
-    url(r'^tags/(?P<tags>(\w+)(,\s*\w+)*)', 'apps.forum.views.get_threads_by_tags', name='get_threads_by_tags'),
+    url(r'^tags/(?P<tags>(.+)(,\s*.+)*)', 'apps.forum.views.get_threads_by_tags', name='get_threads_by_tags'),
 
 
     url(r'^forumpost/create/$', 'apps.forum.views.create_forumpost', name='create_forumpost'),
