15e011439d20360af3449ed0240abc9712391218
Jonas Svensson <jonas.s.svensson@gmail.com>
2012-02-14 00:32:46 +0100

    Small adjustments

--- apps/chat/static/js/chat.js
+++ apps/chat/static/js/chat.js
@@ -14,6 +14,7 @@ var updateChat = function() {
                     formatJsReplyButton();
                     formatImageDialogs();
                     hilightUserName();
+                    bindReplyButton();
                     
                     $('#chat_form textarea, #chat_form input').prop('disabled', false);
                     $('#chat_form input').removeClass('ui-state-hover');
