abe11c5bf2cef59c8c73144adec291d1d1adc6f7
Jonas Svensson <jonas.s.svensson@gmail.com>
2012-02-17 21:37:08 +0100

    pre shemamigrations articles, on live. This is with new fields in ArticleComment

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -6,7 +6,12 @@ class ArticleAdmin(reversion.VersionAdmin):
 
     pass
     
+class ArticleCommentAdmin(reversion.VersionAdmin):
+    
+    pass
+    
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleComment, ArticleCommentAdmin)
 
 admin.site.register(defaultArticleCategories)
 admin.site.register(ModeratorArticleCategories)
