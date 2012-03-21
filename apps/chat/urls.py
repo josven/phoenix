b0ec26753c59cdd7aefd283b833095166ec42e2e
Jonas Svensson <jonas.s.svensson@gmail.com>
2012-03-21 22:45:48 +0100

    cleanup in chat

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -2,19 +2,15 @@
 from django.conf import urls
 
 urlpatterns = patterns('',
+
+    # The chat
     url(r'^$', 'apps.chat.views.chat', name='chat'),
     
     # Get chat, ajax api
     url(r'^get/$', 'apps.chat.views.get_chat', name='get_chat'),
     
-    # Post
-    url(r'^post/$', 'apps.chat.views.chat', name='chat_post'),
-    
-    # Return a single post
-    #url(r'^(?P<id>\d*)\.(?P<type>\w*)$', 'apps.chat.views.chatpost', name='chatpost'),    
-    
-    # Return a range posts
-    #url(r'^(?P<id1>\d*)-(?P<id2>\d*)\.(?P<type>\w*)$', 'apps.chat.views.chatposts', name='chatposts'),
+    # Post chat, ajax api
+    url(r'^post/$', 'apps.chat.views.post_chat', name='chat_post'),
     
     #Tinychat
      url(r'^camchat/$', 'apps.chat.views.camchat', name='camchat'),
